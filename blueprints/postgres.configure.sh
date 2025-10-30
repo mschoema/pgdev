0f12e0851ae718531a513887d9d483dd858fdadf
Maxime Schoemans <maxime.schoemans@enterprisedb.com>
2025-10-30 12:20:15 +0100

    Remove hard-coded config in blueprint
    This removes the configuration options that I use personally from the
    blueprints in the git repo, to make the scripts work out-of-the-box.

--- blueprints/postgres.configure.sh
+++ blueprints/postgres.configure.sh
@@ -19,14 +19,7 @@ tar -xzf "postgresql-$pg_version.tar.gz"
 
 cd "postgresql-$pg_version"
 
-# Link to llvm 20, because postgres doesn't compile with 21 yet
-export LLVM_CONFIG='/opt/homebrew/opt/llvm@20/bin/llvm-config'
-
-# Link to readline manually
-export LDFLAGS="-L$(brew --prefix readline)/lib"
-export CPPFLAGS="-I$(brew --prefix readline)/include"
-
 # Set flags for a release build. -g is included for useful backtraces.
 export CFLAGS="-O2 -g -pipe -march=native"
 
-./configure --prefix="$PGDEV_INSTALL_DIR" --with-lz4 --with-llvm
+./configure --prefix="$PGDEV_INSTALL_DIR" --with-lz4
